a9b1789a37185b66d325d8d4ce43779eea7dee65
dakang <trumpjqk@daum.net>
2023-02-25 04:17:28 +0900

    Feat: 히터블 실린더 관련 구조체 일부 수정

--- includes/hittable.h
+++ includes/hittable.h
@@ -67,30 +67,33 @@ typedef struct s_hittable_plane
 	t_vec3			norm;
 }	t_hittable_plane;
 
-typedef struct s_hittable_cylinder_open
+typedef struct s_hittable_tube
 {
 	t_hittable_hit	hit;
 	t_bounding_box	bounding_box;
 	t_material		*material;
-	double			radius;
+	t_point			center;
+	t_vec3			axis;
 	double			height;
-}	t_hittable_cylinder_open;
+	double			radius;
+}	t_hittable_tube;
 
 typedef struct s_hittable_disk
 {
-	t_hittable_hit	hit;
-	t_bounding_box	bounding_box;
-	t_material		*material;
-	double			radius;
+	t_hittable_hit			hit;
+	t_bounding_box			bounding_box;
+	t_material				*material;
+	t_hittable_plane		plane;
+	double					radius;
 }	t_hittable_disk;
 
 typedef struct s_hittable_cylinder
 {
-	t_hittable_hit				hit;
-	t_bounding_box				bounding_box;
-	t_material					*material;
-	t_hittable_cylinder_open	*side;
-	t_hittable_transform		*caps[2];
+	t_hittable_hit	hit;
+	t_bounding_box	bounding_box;
+	t_material		*material;
+	t_hittable_tube	*tube;
+	t_hittable_disk	*disk[2];
 }	t_hittable_cylinder;
 
 typedef struct s_hittable_list
@@ -101,6 +104,7 @@ typedef struct s_hittable_list
 	t_ptrarr		*elements;
 }	t_hittable_list;
 
+// TODO: 왜 함수가 여기있지..? 확인후 삭제할 것
 t_hittable_sphere	*sphere_create(
 						t_point center,
 						double radius,
