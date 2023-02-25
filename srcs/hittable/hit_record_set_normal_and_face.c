92bf5e42f5491ad46aeba3187c5dd3e8f1fe689a
geneeol <trumpjqk@daum.net>
2023-02-25 22:41:56 +0900

    Chore: 변수명 변경

--- srcs/hittable/hit_record_set_normal_and_face.c
+++ srcs/hittable/hit_record_set_normal_and_face.c
@@ -2,16 +2,16 @@
 #include "ray.h"
 
 void	hit_record_set_normal_and_face(
-			t_hit_record *hit, t_ray *ray, t_vec3 *normal)
+			t_hit_record *rec, t_ray *ray, t_vec3 *normal)
 {
 	if (vec3_dot_vec3(&ray->dir, normal) < 0)
 	{
-		hit->is_front = TRUE;
-		hit->normal = *normal;
+		rec->is_front = TRUE;
+		rec->normal = *normal;
 	}
 	else
 	{
-		hit->is_front = FALSE;
-		vec3_mult_num(&hit->normal, normal, -1);
+		rec->is_front = FALSE;
+		vec3_mult_num(&rec->normal, normal, -1);
 	}
 }
