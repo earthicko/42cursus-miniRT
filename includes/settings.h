53d8bf56b3f233d0d4f17865fa0ab48eacbf4231
Donghyeon Lee <earthicko@gmail.com>
2023-02-26 20:34:45 +0900

    Feat: light 빌드 함수에 필요한 반지름 기본값 명시

--- includes/settings.h
+++ includes/settings.h
@@ -8,7 +8,6 @@
 # define CAM_DEFAULT_CAMUP_Y	1
 # define CAM_DEFAULT_CAMUP_Z	0
 # define CAM_DEFAULT_FOCALLEN	10
-# define RENDERER_MAX_DEPTH	50
-# define RENDERER_N_SAMPLES	1
+# define LIGHT_DEFAULT_R	10
 
 #endif
