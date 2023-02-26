368690863f961710ce28da9f9510634a43cf2981
Donghyeon Lee <earthicko@gmail.com>
2023-02-26 16:45:21 +0900

    Fix: 놈 준수

--- srcs/hittable/hittable_internal.h
+++ srcs/hittable/hittable_internal.h
@@ -13,19 +13,21 @@ enum	e_coefficient
 t_bool	val_is_near_zero(double val);
 double	dist_sq(t_point *a, t_point *b);
 t_bool	solve_quadratic_equation(t_minmax t, double coef[3], double *root);
-t_bool	solve_equation_system_plane_and_line(t_minmax t, \
-											t_hittable_plane *plane, \
-											t_ray *ray, \
-											double *root);
+t_bool	solve_equation_system_plane_and_line(
+			t_minmax t,
+			t_hittable_plane *plane,
+			t_ray *ray,
+			double *root);
 
+void	set_tube_of_cylinder(
+			t_hittable_tube *tube,
+			t_cylinder_info *cylinder_info,
+			t_material *material);
 
-void	set_tube_of_cylinder(t_hittable_tube *tube, \
-									t_cylinder_info *cylinder_info, \
-									t_material *material);
-
-void	set_disk_of_cylinder(t_hittable_disk *disk, \
-									t_cylinder_info *cylinder_info, \
-									t_material *material, \
-									int disk_type);
+void	set_disk_of_cylinder(
+			t_hittable_disk *disk,
+			t_cylinder_info *cylinder_info,
+			t_material *material,
+			int disk_type);
 
 #endif
