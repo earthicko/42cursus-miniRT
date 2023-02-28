01a1714c0ed1f783081357427a7a0353e6e8a8a3
Donghyeon Lee <earthicko@gmail.com>
2023-02-28 13:43:58 +0900

    Feat: matrix44 출력 함수 추가

--- includes/print.h
+++ includes/print.h
@@ -9,6 +9,7 @@
 void	print_vec2(const t_vec2 *vec2);
 void	print_vec3(const t_vec3 *vec3);
 void	print_ray(const t_ray *ray);
+void	print_mtx44(const t_mtx44 *mtx44);
 void	print_camera(const t_camera *cam);
 void	print_hit_record(const t_hit_record *rec);
 void	print_scatter_record(const t_scatter_record *rec);
