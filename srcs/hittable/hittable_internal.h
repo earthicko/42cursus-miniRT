8febdf9f263aa4cbb4e7884f927a5cef13395fc2
geneeol <trumpjqk@daum.net>
2023-02-28 17:25:40 +0900

    Feat: cone set함수 헤더 추가

--- srcs/hittable/hittable_internal.h
+++ srcs/hittable/hittable_internal.h
@@ -18,20 +18,28 @@ t_bool	solve_quadratic_equation(t_minmax t, double coef[3], double *root);
 t_bool	solve_equation_system_plane_and_line(const t_minmax t, \
 												const t_hittable_plane *plane, \
 												const t_ray *ray, \
-			double *root);
+												double *root);
 
 
 /******* Set cylinder's elements *******/
 
 void	set_tube_of_cylinder(t_hittable_tube *tube, \
 								t_cylinder_info *cylinder_info, \
-			t_material *material);
+								t_material *material);
 
 void	set_disk_of_cylinder(t_hittable_disk *disk, \
 								t_cylinder_info *cylinder_info, \
 								t_material *material, \
-			int disk_type);
+								int disk_type);
 
 
 /******* Set cones's elements *******/
+
+void	set_disk_of_cone(t_hittable_disk *disk, \
+							t_cone_info *cone_info, \
+							t_material *material);
+
+void	set_conical_hat_of_cylinder(t_hittable_conical_hat *conical_hat, \
+									t_cone_info *cone_info, \
+									t_material *material);
 #endif
