dfb73a7e0c42ddbb15178f6a88d73322d1bd4551
Donghyeon Lee <earthicko@gmail.com>
2023-02-28 15:38:11 +0900

    Fix: 파싱 시 단위벡터 검증 로직 고침

--- srcs/parser/builder/checkrange.c
+++ srcs/parser/builder/checkrange.c
@@ -46,22 +46,14 @@ t_bool	is_invalid_color(const t_color *color)
 
 t_bool	is_invalid_normalized_vec3(const t_vec3 *vec)
 {
-	int	i;
+	double	len;
 
-	i = 0;
-	while (i < 3)
-	{
-		if (!(RANGE_MIN_NORMALIZEDVEC3 <= vec->i[i]
-				&& vec->i[i] <= RANGE_MAX_NORMALIZEDVEC3))
-		{
-			ft_dprintf(2, "%s: normalized vector component "MSG_OUTOFRANGE"\n",
-				EXEC_NAME,
-				vec->i[i], RANGE_MIN_NORMALIZEDVEC3, RANGE_MAX_NORMALIZEDVEC3);
-			return (TRUE);
-		}
-		i++;
-	}
-	return (FALSE);
+	len = vec3_get_len(vec);
+	if (1.f - E_NORMALIZEDVEC3 < len && len < 1.f + E_NORMALIZEDVEC3)
+		return (FALSE);
+	printf("%s: vector length "MSG_OUTOFRANGE"\n", EXEC_NAME,
+		len, 1.f - E_NORMALIZEDVEC3, 1.f + E_NORMALIZEDVEC3);
+	return (TRUE);
 }
 
 t_bool	is_invalid_fov(double fov)
