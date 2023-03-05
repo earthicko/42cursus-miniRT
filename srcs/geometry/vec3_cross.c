63497c1878c0b39a95accf6bcdbdafe06fe6713c
earth <earthicko@gmail.com>
2023-03-05 13:59:24 +0900

    Fix: vec3 cross 부호 변경사항 되돌림
    - 9107a8cc6d318d24f5a04a38e5761730d102e4ff 롤백

--- srcs/geometry/vec3_cross.c
+++ srcs/geometry/vec3_cross.c
@@ -1,13 +1,6 @@
 #include "libft.h"
 #include "geometry.h"
 
-// TODO: 외적 수정하면서 생긴 부수적인 문제 해결할 것
-// A 0.02 255,255,255
-// C 40,0.0, 20   -1, 0, 0   120
-// l -40.0,50.0,0.0   0.6    10,0,255
-// cy 0.0,0.0,0.0     0.0,0.0,1.0  20  20  0,0,255
-// 원기둥이 이상하게 보이는듯?
-
 void	vec3_cross_vec3(t_vec3 *out, const t_vec3 *a, const t_vec3 *b)
 {
 	int	i;
@@ -17,8 +10,6 @@ void	vec3_cross_vec3(t_vec3 *out, const t_vec3 *a, const t_vec3 *b)
 	{
 		out->i[i] = a->i[(i + 1) % 3] * b->i[(i + 2) % 3]
 			- a->i[(i + 2) % 3] * b->i[(i + 1) % 3];
-		if (i % 2 == 1)
-			out->i[i] *= -1;
 		i++;
 	}
 }
