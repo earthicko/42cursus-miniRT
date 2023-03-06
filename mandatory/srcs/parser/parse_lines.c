b771c8a7b124c5ec67555e2b7c68d51c006fb8b6
Donghyeon Lee <earthicko@gmail.com>
2023-03-06 21:31:45 +0900

    Chore: 과도하게 별도의 파일로 나누어진 기능을 통합

--- mandatory/srcs/parser/parse_lines.c
+++ mandatory/srcs/parser/parse_lines.c
@@ -9,6 +9,30 @@ int		parse_unique_entity_loop(
 int		parse_common_entity(const char *line, t_scene *scene);
 t_bool	all_unique_entities_found(t_bool *map);
 
+/*
+	returns TRUE if parsing was successful, FALSE if wasn't,
+	negative if error occurs.
+*/
+int	parse_line(const char *line, t_scene *scene,
+	t_patternmatcher tester, t_builder builder)
+{
+	t_ptrarr	*tokens;
+	int			stat;
+
+	tokens = tokenize(line);
+	if (!tokens)
+		return (CODE_ERROR_MALLOC);
+	stat = FALSE;
+	if (tester(tokens))
+	{
+		stat = builder(tokens, scene);
+		if (!stat)
+			stat = TRUE;
+	}
+	ptrarr_destroy(tokens, destroy_pchar);
+	return (stat);
+}
+
 int	parse_lines(const t_ptrarr *lines, t_scene *scene)
 {
 	int		i;
